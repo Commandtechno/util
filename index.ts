ae70185560041e9ba63228927e0ac6375ec2fb0d
Xavier B <facedepete@live.ca>
2021-10-20 19:42:28 -0400

    remove useless semicolon

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import * as fs from 'fs';
 export function parseEmoji(emoji) {
   const [_, name, id] = emoji.slice(1, -1).split(":");
   return ":" + name + ":";
-};
+}
 
 export function isDirectory() {
   return fs.lstatSync(path).isDirectory();
