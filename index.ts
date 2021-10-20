d0371595b231c5efb06f85d733b58b231d9d194c
Daedalus <redstonedaedalus@gmail.com>
2021-10-20 17:24:17 -0600

    Fix formatting

--- index.ts
+++ index.ts
@@ -1,11 +1,11 @@
 import * as fs from 'fs';
 
-export function parseEmoji (emoji) {
+export function parseEmoji(emoji) {
   const [_, name, id] = emoji.slice(1, -1).split(":");
   return ":" + name + ":";
 };
 
-export function isDirectory () {
+export function isDirectory() {
   return fs.lstatSync(path).isDirectory();
 }
 
